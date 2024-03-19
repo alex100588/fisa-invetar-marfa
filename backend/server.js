cddbecb9e406f2f372b56f73ddea4ac5968d5091
alex100588 <49831245+alex100588@users.noreply.github.com>
2024-03-19 17:55:37 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
 require('dotenv').config()
 
 const express = require('express')
+const marfaRute = require('./routes/marfa')
 
 // express app
 const app = express()
 
 // middleware
+app.use(express.json())
+
 app.use((req, res, next) =>{
     console.log(req.path, req.method);
     next()
 })
 
-app.get('/', (req, res)=>{
-    res.json({mssg: 'Welcome to the app'})
-})
+app.use('/api/marfa', marfaRute)
 
 
 // listen for requests
